492027d7b6d1137a704c7257a99bffc922c183a5
Angel Parra <arpr18@gmail.com>
2022-03-21 13:13:25 -0500

    Actualizacion Submodulos

--- web_ir_actions_act_multi/__manifest__.py
+++ web_ir_actions_act_multi/__manifest__.py
@@ -11,8 +11,8 @@
     "version": "14.0.1.0.0",
     "license": "LGPL-3",
     "author": "Modoolar, " "CorporateHub, " "Odoo Community Association (OCA)",
-    "website": "https://github.com/OCA/web/",
+    "website": "https://github.com/OCA/web",
     "depends": ["web"],
-    "data": ["views/web_ir_actions_act_multi.xml"],
+    "data": ["security/ir.model.access.csv", "views/web_ir_actions_act_multi.xml"],
     "installable": True,
 }
